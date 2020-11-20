0543761af5c1e1c1b438d9a13e6603ce7b45bdd4
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-19 22:11:52 -0800

    done

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    new_song.artist = self
+  end
+
+  def self.song_count
+    @@all.sum{|artist| artist.songs.length}
+  end
+
+end
+
